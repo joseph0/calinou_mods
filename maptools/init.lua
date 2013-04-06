a38270f9e2aa37122e9d068fb41d0f29ca35e79e
Calinou <calinou9999spam@gmail.com>
2013-04-06 18:43:18 +0200

    Add an admin pickaxe with drops

--- maptools/init.lua
+++ maptools/init.lua
@@ -387,6 +387,25 @@ minetest.register_tool("maptools:pick_admin", {
 	},
 })
 
+minetest.register_tool("maptools:pick_admin_with_drops", {
+	description = S("Admin Pickaxe With Drops"),
+	inventory_image = "maptools_adminpick.png",
+	groups = {not_in_creative_inventory=1},
+	tool_capabilities = {
+		full_punch_interval = 0,
+		max_drop_level=3,
+		groupcaps= {
+			unbreakable={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+			fleshy = {times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+			choppy={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+			bendy={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+			cracky={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+			crumbly={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+			snappy={times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+		}
+	},
+})
+
 minetest.register_on_punchnode(function(pos, node, puncher)
 	if puncher:get_wielded_item():get_name() == "maptools:pick_admin"
 	and minetest.env: get_node(pos).name ~= "air" then
